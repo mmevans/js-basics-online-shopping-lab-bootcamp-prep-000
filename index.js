19fc3573acec57645ce159c6fba00aaf39c18290
Michael Evans <mmevansjr@gmail.com>
2019-04-12 15:03:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ function addToCart(item) {
 }
 
 function viewCart() {
+  var array = [];
+  for(let i = 0; i < array.length; i++) {
+    
+  }
   if(cart.length === 0) {
     return("Your shopping cart is empty.");
   } else if (cart.length === 1) {
