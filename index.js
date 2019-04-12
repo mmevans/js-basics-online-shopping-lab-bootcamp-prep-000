ca02a0aafb5dcdf766ebe358ae60ca6017769488
Michael Evans <mmevansjr@gmail.com>
2019-04-12 15:21:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ function total() {
   let totalPrice = [];
   for(let i = 0; i < totalPrice.length; i++) {
     totalPrice.push(`${cart[i].itemPrice}`);
+    console.log(totalPrice.reduce)
     
   }
 }
