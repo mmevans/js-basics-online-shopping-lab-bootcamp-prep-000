ef86f763da0546086e7e2c002171b52080cb9d53
Michael Evans <mmevansjr@gmail.com>
2019-04-12 14:27:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var newItem = {[item]: Math.floor(Math.random(1, 101) * 100)};
+  var newItem = {[itemName]: item};
   cart.push(newItem);
   console.log(`${item} has been added to your cart`);
 }
