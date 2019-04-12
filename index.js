42dc63e45380a0e2dd3ab923b75f9b2044779917
Michael Evans <mmevansjr@gmail.com>
2019-04-12 14:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ function addToCart(item) {
 function viewCart() {
   if(cart.length === 0) {
     return("Your shopping cart is empty.");
-  } else if (cart.length === 1) {
-    return(`In your cart, you have ` + newItem[itemName] + ` at $` + newPrice[itemPrice] + `.`) 
   }
 }
 
